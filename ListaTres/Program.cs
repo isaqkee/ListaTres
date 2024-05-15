41a07346d3f99cf4673ff176f59bc433eef0fdee
isaqkee <141652864+isaqkee@users.noreply.github.com>
2024-05-15 15:54:11 -0300

    Lista 3 - C# Finalizada

---
+++ ListaTres/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        /*ATIVIDADE 1*/
+        /*int numeroFuncionario;
+        double horasTrabalhadas;
+        double valorPorHora;
+        double salario;
+
+        
+        Console.Write("Digite o número do funcionário: ");
+        numeroFuncionario = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o número de horas trabalhadas: ");
+        horasTrabalhadas = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor que o funcionário recebe por hora: ");
+        valorPorHora = double.Parse(Console.ReadLine());
+
+        
+        salario = horasTrabalhadas * valorPorHora;
+
+        
+        Console.WriteLine($"Número do funcionário: {numeroFuncionario}");
+        Console.WriteLine($"Salário: R$ {salario:F2}");*/
+
+        /*ATIVIDADE 2*/
+
+        /*int codigoPeca1, numeroPecas1;
+        double valorUnitarioPeca1, totalPeca1;
+
+        int codigoPeca2, numeroPecas2;
+        double valorUnitarioPeca2, totalPeca2;
+
+        double valorTotal;
+
+        
+        Console.Write("Digite o código da peça 1: ");
+        codigoPeca1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o número de peças 1: ");
+        numeroPecas1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor unitário da peça 1: ");
+        valorUnitarioPeca1 = double.Parse(Console.ReadLine());
+
+        
+        Console.Write("Digite o código da peça 2: ");
+        codigoPeca2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o número de peças 2: ");
+        numeroPecas2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor unitário da peça 2: ");
+        valorUnitarioPeca2 = double.Parse(Console.ReadLine());
+
+       
+        totalPeca1 = numeroPecas1 * valorUnitarioPeca1;
+        totalPeca2 = numeroPecas2 * valorUnitarioPeca2;
+
+        
+        valorTotal = totalPeca1 + totalPeca2;
+
+        Console.WriteLine($"Valor a ser pago: R$ {valorTotal:F2}");*/
+
+        /*ATIVIDADE 3*/
+
+        /* double A, B, C;
+
+            
+            Console.Write("Digite o valor de A: ");
+            A = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de B: ");
+            B = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de C: ");
+            C = double.Parse(Console.ReadLine());
+
+            
+            double areaTriangulo = A * C / 2.0;
+            double areaCirculo = 3.14159 * C * C;
+            double areaTrapezio = (A + B) / 2.0 * C;
+            double areaQuadrado = B * B;
+            double areaRetangulo = A * B;
+
+            
+            Console.WriteLine($"a) Área do triângulo retângulo: {areaTriangulo:F2}");
+            Console.WriteLine($"b) Área do círculo: {areaCirculo:F2}");
+            Console.WriteLine($"c) Área do trapézio: {areaTrapezio:F2}");
+            Console.WriteLine($"d) Área do quadrado: {areaQuadrado:F2}");
+            Console.WriteLine($"e) Área do retângulo: {areaRetangulo:F2}");*/
+
+        /*LISTA 3.5 C#*/
+
+        /*ATIVIDADE 4*/
+
+        /*string login, senha;
+
+        Console.Write("Digite o login: ");
+        login = Console.ReadLine();
+
+        Console.Write("Digite a senha: ");
+        senha = Console.ReadLine();
+
+        
+        if (login == "admin" && senha == "1234")
+        {
+            Console.WriteLine("Pode acessar");
+        }
+        else
+        {
+            Console.WriteLine("Login ou senha incorreto");
+        }*/
+
+        /*ATIVIDADE 5*/
+
+        /*int quantidadeBlusas;
+
+        
+        Console.Write("Digite a quantidade de blusas no estoque: ");
+        quantidadeBlusas = int.Parse(Console.ReadLine());
+
+        
+        if (quantidadeBlusas > 40)
+        {
+            Console.WriteLine("ESTOQUE OK");
+        }
+        else if (quantidadeBlusas > 20 && quantidadeBlusas <= 40)
+        {
+            Console.WriteLine("ATENÇÃO AO ESTOQUE");
+        }
+        else
+        {
+            Console.WriteLine("ESTOQUE CRITICO");*/
+
+        /*ATIVIDADE 6*/
+
+        /*int idade;
+        double salario;
+
+        Console.Write("Digite a sua idade: ");
+        idade = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o seu salário: ");
+        salario = double.Parse(Console.ReadLine());
+
+        if (idade >= 18 && salario > 2000)
+        {
+            Console.WriteLine("PODE VIAJAR");
+        }
+        else
+        {
+            Console.WriteLine("NÃO PODE VIAJAR");*/
+
+        /*ATIVIDADE 7*/
+
+        /*int opcao;
+
+        Console.Write("Digite o número da aula escolhida (1 a 4 para opções específicas, qualquer outro número para dança): ");
+        opcao = int.Parse(Console.ReadLine());
+
+        if (opcao == 1 || opcao == 2)
+        {
+            Console.WriteLine("DESENHO E PINTURA");
+        }
+        else if (opcao == 3 || opcao == 4)
+        {
+            Console.WriteLine("DISCURSO E DEBATE");
+        }
+        else
+        {
+            Console.WriteLine("DANÇA");*/
+
+        /*ATIVIDADE 8
+
+        /*int numero;
+
+        Console.Write("Digite um número: ");
+        numero = int.Parse(Console.ReadLine());
+
+        if (numero > 0 && numero > 100)
+        {
+            Console.WriteLine("número especial");
+        }
+        else
+        {
+            Console.WriteLine("fora do intervalo");*/
+        }
+    }
+
